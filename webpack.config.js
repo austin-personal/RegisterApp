f4505f1a8621ee5aa039f30450657ae2f8cdb3e5
changmo <kmaestro043@gmail.com>
2024-01-21 19:06:16 +1300

    update workflow

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = { //exporting a JavaScript object as a module
+  entry: './app.js', //main JavaScript file to start bundling dependencies
+  output: { //output store as
+    filename: 'bundle.js', 
+    path: path.resolve(__dirname, 'dist'), //location to store
+  },
+  module: { //set up rules how webpack should process different types of files
+    rules: [
+      {
+        test: /\.css$/, //regex to find .css file
+        use: ['style-loader', 'css-loader'], //tool to process
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './Public/index.html', //Specifies an array of webpack plugins to enhance the webpack functionality
+      filename: 'index.html',
+    }),
+  ],
+};
