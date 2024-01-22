af043558a7bd62ff29fa1993ee94c6c45f8ef7c3
changmo <kmaestro043@gmail.com>
2024-01-22 16:27:11 +1300

    Update workflow - Add async = false

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,13 @@ module.exports = { //exporting a JavaScript object as a module
     fallback: {
       path: require.resolve("path-browserify"),
       stream: require.resolve("stream-browserify"),
-      async_hooks: require.resolve("async_hooks"),
       zlib: require.resolve('browserify-zlib'),
       crypto: require.resolve('crypto-browserify'),
       fs: false,
       http: require.resolve('stream-http'),
       assert: require.resolve('assert/'),
       net: require.resolve('net'),
+      async_hooks: false,
     },
   },
   module: { //set up rules how webpack should process different types of files
