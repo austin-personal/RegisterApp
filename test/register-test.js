2626d0ce58fec685089c39eca92c6897cc109b7c
changmo <kmaestro043@gmail.com>
2024-02-01 16:14:05 +1300

    Update workflow

--- test/register-test.js
+++ test/register-test.js
@@ -1,15 +1,14 @@
-// Use dynamic import
 const { expect } = import('chai');
 
 const axios = require('axios');
 
 describe('Registration Functionality', () => {
   it('should register a user successfully', async () => {
-    const response = await axios.post('http://localhost:8080/register', {
-      name: 'TestUser',
-      gender: 'Male',
+    const response = await axios.post('http://localhost:3000/register', {
+      name: '최효정',
+      gender: 'Female',
     });
-
+    console.log(response.status)
     expect(response.status).to.equal(200);
     // Add more assertions as needed
   });
